0276badad1c3fed60d1f8722b0b5acb9272b3914
rokayon <141338833+rokayon@users.noreply.github.com>
2025-02-05 13:58:26 -0600

    Update __init__.py

--- rokon/__init__.py
+++ rokon/__init__.py
@@ -1,4 +1,4 @@
-# __init__.py
+# rokon/__init__.py
 
 from .RNet import *  # Import all models dynamically from RNet.py
-from .model_loader import get_rokon_model  # Import the dynamic loader function
+from .model_loader import get_rokon_model  # Import the correct function
