28285a6dce083014583ef8b2728edd198c4a5b2e
Rokonozzaman Ayon <you@example.com>
2025-02-05 11:51:43 -0600

    Updated setup.py for PyPI upload and GitHub integration

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup, find_packages 
+from setuptools import setup, find_packages
+
 setup(
-name="rokon", 
-version="0.1", 
-packages=find_packages(), 
-install_requires=[ 
-'tensorflow', 
-], 
-description="A lightweight CNN model repository", 
-author="rokayon", 
-author_email="rokayon.cse@gmail.com", 
-) 
+    name='rokon',                          # The name of the package
+    version='0.1.0',                       # Version of the package
+    packages=find_packages(),              # Automatically find all the packages inside 'rokon' folder
+    install_requires=[                     # External dependencies
+        'tensorflow',                      # Include TensorFlow if necessary
+        'numpy',                            # Include numpy if necessary
+    ],
+    # Other metadata
+    author='Rokonozzaman Ayon',
+    author_email='rokayon.cse@gmail.com',
+    description='A package for RNet models and dynamic model loading',
+    url='https://github.com/rokayon/rokon',  # Your GitHub URL (optional)
+)
