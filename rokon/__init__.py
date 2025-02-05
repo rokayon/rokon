425aac5b017332de7096c515e89da7555b876bed
Rokonozzaman Ayon <you@example.com>
2025-02-05 11:52:23 -0600

    Updated setup.py

--- rokon/__init__.py
+++ rokon/__init__.py
@@ -1,1 +1,2 @@
-from .RNet import RNet1, RNet2, RNet3, RNet4, RNet5
+from .RNet import RNet1, RNet2, RNet3, RNet4, RNet5  # Import all models
+from .model_loader import load_model  # Import the dynamic loader
