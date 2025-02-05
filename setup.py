1e5dbcb31f811a4e56c9701eed59330fd8c1eae8
Rokonozzaman Ayon <you@example.com>
2025-02-05 05:44:48 -0600

    Added setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages 
+setup(
+name="rokon", 
+version="0.1", 
+packages=find_packages(), 
+install_requires=[ 
+'tensorflow', 
+], 
+description="A lightweight CNN model repository", 
+author="rokayon", 
+author_email="rokayon.cse@gmail.com", 
+) 
