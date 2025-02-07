096f266d70b623b6bd7858f88d672805b8a4b782
rokayon <141338833+rokayon@users.noreply.github.com>
2025-02-07 08:39:10 -0600

    Update RNet.py

--- rokon/RNet.py
+++ rokon/RNet.py
@@ -66,6 +66,51 @@ def RNet5(input_shape, num_classes, activation="relu"):
     ])
     return model
 
+def RNet6(input_shape, num_classes, activation="relu"):
+    model = models.Sequential([
+        layers.DepthwiseConv2D((3, 3), activation=activation, input_shape=input_shape),
+        layers.Conv2D(32, (1, 1), activation=activation),
+        layers.MaxPooling2D(2, 2),
+        layers.DepthwiseConv2D((3, 3), activation=activation),
+        layers.Flatten(),
+        layers.Dense(128, activation=activation),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    return model
+def RNet7(input_shape, num_classes, activation="relu"):
+    model = models.Sequential([
+        layers.Conv2D(64, (3, 3), activation=activation, input_shape=input_shape),
+        layers.MaxPooling2D(2, 2),
+        layers.Flatten(),
+        layers.Dense(128, activation=activation),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    return model
 
+def RNet8(input_shape, num_classes, activation="relu"):
+    model = models.Sequential([
+        layers.DepthwiseConv2D((3, 3), activation=activation, input_shape=input_shape),
+        layers.Conv2D(32, (1, 1), activation=activation),
+        layers.MaxPooling2D(2, 2),
+        layers.DepthwiseConv2D((3, 3), activation=activation),
+        layers.Flatten(),
+        layers.Dense(128, activation=activation),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    return model
+
+def RNet9(input_shape, num_classes, activation="relu"):
+    model = models.Sequential([
+        layers.Conv2D(32, (3,3), activation=activation, input_shape=input_shape),
+        layers.Conv2D(32, (1,1), activation=activation),
+        layers.MaxPooling2D(2,2),
+        layers.Conv2D(64, (3,3), activation=activation),
+        layers.Conv2D(64, (1,1), activation=activation),
+        layers.MaxPooling2D(2,2),
+        layers.Flatten(),
+        layers.Dense(128, activation=activation),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    return model
 # Add this at the END of the file to allow wildcard imports
-__all__ = ["RNet1", "RNet2", "RNet3", "RNet4", "RNet5"]
+__all__ = ["RNet1", "RNet2", "RNet3", "RNet4", "RNet5", "RNet6", "RNet7", "RNet8", "RNet9"]
